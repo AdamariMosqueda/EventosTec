9c363806f6c987da068251413072fb7c3f4f7efa
AdamariMosqueda <adamari.mosqueda16@tectijuana.edu.mx>
2020-05-13 16:40:03 -0700

    User

--- EventosTec/EventosTec.Web/Models/DataDbContext.cs
+++ EventosTec/EventosTec.Web/Models/DataDbContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventosTec.Web.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventosTec.Web.Models
 {
-    public class DataDbContext: DbContext
+    public class DataDbContext: IdentityDbContext<User>
     {
         public DataDbContext (DbContextOptions<DataDbContext> options): base(options)
         {
@@ -16,7 +17,7 @@ namespace EventosTec.Web.Models
         public DbSet<Client> Clients { get; set; }
         public DbSet<City> Clities { get; set; }
         public DbSet<Event> Events { get; set; }
-
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Manager> Managers { get; set; }
     }
 }
